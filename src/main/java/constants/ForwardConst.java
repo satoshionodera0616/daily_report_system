6346ac126c666fed7361b6b14fe8de73869fcb30
satoshi.onodera <ousyo@MyComputer>
2021-10-01 13:49:13 +0900

    従業員editアクションの作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -36,7 +36,7 @@ public enum ForwardConst {
     FW_TOP_INDEX("topPage/index"),
     FW_LOGIN("login/login"),
     FW_EMP_INDEX("employees/index"),
-    FW_EMP_SHOW("emploeyees/show"),
+    FW_EMP_SHOW("employees/show"),
     FW_EMP_NEW("employees/new"),
     FW_EMP_EDIT("employees/edit"),
     FW_REP_INDEX("reports/index"),
